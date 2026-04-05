b6516768ea0badf0a0dedfe3e31532716136c66e
Amin Ziagham <amin.ziagham@outlook.com>
2026-04-05 21:57:45 +0200

    Add ApplicationErrorCodes and ApplicationException classes
    Introduce ApplicationErrorCodes with AgentNotFound and InvalidClientSession error codes, and add an abstract ApplicationException base class for consistent error handling in the application layer. Both utilize Vectra.BuildingBlocks for error and exception management.
    
    #67

---
+++ src/Vectra.Application/Errors/ApplicationErrorCodes.cs
@@ -0,0 +1,9 @@
+﻿using Vectra.BuildingBlocks.Errors;
+
+namespace Vectra.Application.Errors;
+
+public static class ApplicationErrorCodes
+{
+    public static readonly ErrorCode AgentNotFound = new(0501002, ErrorCategory.Persistence);
+    public static readonly ErrorCode InvalidClientSession = new(0800003, ErrorCategory.Security);
+}
---
+++ src/Vectra.Application/Exceptions/ApplicationException.cs
@@ -0,0 +1,11 @@
+﻿using Vectra.BuildingBlocks.Errors;
+using Vectra.BuildingBlocks.Exceptions;
+
+namespace Vectra.Application.Exceptions;
+
+public abstract class ApplicationException : BaseException
+{
+    protected ApplicationException(ErrorCode errorCode, string message) : base(errorCode, message)
+    {
+    }
+}
