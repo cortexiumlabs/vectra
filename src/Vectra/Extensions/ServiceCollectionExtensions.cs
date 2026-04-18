2ea16bde1953c46328805c6a6234f11e3b149b15
Amin Ziagham <amin.ziagham@outlook.com>
2026-04-19 00:37:03 +0200

    Remove '--start' argument check and related exception
    Removed ParseVectraArguments method, StartArgumentRequiredException, and the associated error code, simplifying argument handling and error management.
    
    #158

--- src/Vectra/Extensions/ServiceCollectionExtensions.cs
+++ src/Vectra/Extensions/ServiceCollectionExtensions.cs
@@ -103,17 +103,6 @@ public static class ServiceCollectionExtensions
 
     #region Arguments / Version helpers
 
-    public static IServiceCollection ParseVectraArguments(this IServiceCollection services, string[] args)
-    {
-        var hasStartArgument = args.Contains("--start");
-        if (!hasStartArgument)
-        {
-            throw new StartArgumentRequiredException();
-        }
-
-        return services;
-    }
-
     public static bool HandleVersionFlag(this string[] args)
     {
         if (args.Any(arg => arg.Equals("--version", StringComparison.OrdinalIgnoreCase) ||
