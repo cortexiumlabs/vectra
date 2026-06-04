c58a19c42fe95cd0e0d7b5536f5051d66a4d809a
Amin Ziagham <amin.ziagham@outlook.com>
2026-06-04 22:30:48 +0200

    Rename parameter in Dispatch method to 'action'
    Changed the parameter name in IDispatcher.Dispatch from 'request' to 'action' for improved clarity and consistency with IAction<TResult>. No functional changes were made.
    
    #284

--- src/Vectra.Application/Abstractions/Dispatchers/IDispatcher.cs
+++ src/Vectra.Application/Abstractions/Dispatchers/IDispatcher.cs
@@ -2,5 +2,5 @@
 
 public interface IDispatcher
 {
-    Task<TResult> Dispatch<TResult>(IAction<TResult> request, CancellationToken cancellationToken = default);
+    Task<TResult> Dispatch<TResult>(IAction<TResult> action, CancellationToken cancellationToken = default);
 }
