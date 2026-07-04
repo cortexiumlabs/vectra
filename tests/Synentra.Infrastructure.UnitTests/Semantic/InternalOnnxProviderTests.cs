1029a5163bcb64a46448e7f0cdd48dd62aa99aa8
Amin Ziagham <amin.ziagham@outlook.com>
2026-07-04 18:05:13 +0200

    Add unit tests for InternalOnnxProvider edge cases
    Added tests for constructor argument validation, cache access on null/whitespace input, Dispose idempotency, and disabled provider behavior in InternalOnnxProviderTests.
    
    #346

--- tests/Synentra.Infrastructure.UnitTests/Semantic/InternalOnnxProviderTests.cs
+++ tests/Synentra.Infrastructure.UnitTests/Semantic/InternalOnnxProviderTests.cs
@@ -124,4 +124,103 @@ public class InternalOnnxProviderTests
 
         act.Should().Throw<FileNotFoundException>();
     }
+
+    [Fact]
+    public void Constructor_NullLogger_ThrowsArgumentNullException()
+    {
+        var act = () => new InternalOnnxProvider(
+            DisabledOptions(),
+            _cacheService,
+            null!);
+
+        act.Should().Throw<ArgumentNullException>()
+            .WithParameterName("logger");
+    }
+
+    [Fact]
+    public void Constructor_EnabledWithoutCacheProvider_ThrowsArgumentNullException()
+    {
+        var cacheService = Substitute.For<ICacheService>();
+        cacheService.Current.Returns((ICacheProvider?)null);
+
+        var options = Options.Create(new SemanticConfiguration
+        {
+            Enabled = true,
+            Providers = new SemanticProviders
+            {
+                Internal = new InternalOnnxConfiguration
+                {
+                    PackagePath = "dummy.zip"
+                }
+            }
+        });
+
+        var act = () => new InternalOnnxProvider(
+            options,
+            cacheService,
+            NullLogger<InternalOnnxProvider>.Instance);
+
+        act.Should().Throw<ArgumentNullException>();
+    }
+
+    [Fact]
+    public async Task AnalyzeAsync_WhitespaceBody_DoesNotAccessCache()
+    {
+        var sut = new InternalOnnxProvider(
+            DisabledOptions(),
+            _cacheService,
+            NullLogger<InternalOnnxProvider>.Instance);
+
+        await sut.AnalyzeAsync("   ", "", CancellationToken.None);
+
+        await _cacheProvider
+            .DidNotReceiveWithAnyArgs()
+            .TryGetValueAsync<object>(default!);
+    }
+
+    [Fact]
+    public async Task AnalyzeAsync_NullBody_DoesNotAccessCache()
+    {
+        var sut = new InternalOnnxProvider(
+            DisabledOptions(),
+            _cacheService,
+            NullLogger<InternalOnnxProvider>.Instance);
+
+        await sut.AnalyzeAsync(null, "", CancellationToken.None);
+
+        await _cacheProvider
+            .DidNotReceiveWithAnyArgs()
+            .TryGetValueAsync<object>(default!);
+    }
+
+    [Fact]
+    public void Dispose_CanBeCalledTwice()
+    {
+        var sut = new InternalOnnxProvider(
+            DisabledOptions(),
+            _cacheService,
+            NullLogger<InternalOnnxProvider>.Instance);
+
+        sut.Dispose();
+
+        var act = () => sut.Dispose();
+
+        act.Should().NotThrow();
+    }
+
+    [Fact]
+    public async Task AnalyzeAsync_DisabledProvider_IgnoresMetadata()
+    {
+        var sut = new InternalOnnxProvider(
+            DisabledOptions(),
+            _cacheService,
+            NullLogger<InternalOnnxProvider>.Instance);
+
+        var result = await sut.AnalyzeAsync(
+            "hello",
+            Guid.NewGuid().ToString(),
+            CancellationToken.None);
+
+        result.Intent.Should().Be("suspicious");
+    }
 }
